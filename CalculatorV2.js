aa3c103795156c6cb9a79fd281012771e2567676
Stack Quantum <stackquantum1@gmail.com>
2025-11-27 16:39:15 -0800

    Javascript file loaded

---
+++ CalculatorV2.js
@@ -0,0 +1,9 @@
+//  Element Selectors
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const topDisplayTextElement = document.querySelector("[data-top-display]");
+const bottomDisplayTextElement = document.querySelector("[data-bottom-display]");
