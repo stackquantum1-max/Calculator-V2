f6dfa6de5413b07fd1eb9cdf22c1b77536d989f8
Stack Quantum <stackquantum1@gmail.com>
2025-11-27 16:44:23 -0800

    Function Added

--- CalculatorV2.js
+++ CalculatorV2.js
@@ -19,3 +19,33 @@ class Calculator {
 
 //  Instantiate Calculator
 const calculator = new Calculator(topDisplayTextElement, bottomDisplayTextElement);
+
+//  Event Listeners
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  })
+);
+
+operationButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  })
+);
+
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+allClearButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
